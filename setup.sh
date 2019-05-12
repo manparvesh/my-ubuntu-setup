cdc7623c0fc94e804c4e6bc319e1535aca5daf9b
Man Parvesh Singh Randhawa <manparvesh@users.noreply.github.com>
2019-05-13 01:02:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,66 +3,22 @@
 #run after changing permissions by using 'chmod u+x' on this file
 
 # add PPAs
-sudo add-apt-repository ppa:webupd8team/brackets -y
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo add-apt-repository ppa:budgie-remix/ppa -y
-sudo add-apt-repository ppa:webupd8team/atom -y
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 
 # update after adding PPAs
 sudo apt-get update
 
-############################## UI ##############################
-# budgie-remix
-sudo apt install budgie-desktop-environment -y
-
-# guake terminal
-sudo apt-get install guake -y
-
-# cairo-dock
-sudo apt-get install cairo-dock -y
-
-# ultra-flat orange icons
-cd && wget -O Ultra-Flat-Orange.deb https://launchpad.net/~noobslab/+archive/ubuntu/icons/+files/ultra-flat-icons-orange_1.3.2~trusty~Noobslab.com_all.deb
-sudo dpkg -i Ultra-Flat-Orange.deb;rm Ultra-Flat-Orange.deb
+sudo apt-get install chrome-gnome-shell
 
 # tweak tool
 sudo apt-get install gnome-tweak-tool -y
 
-# macubuntu icons
-############################## / UI ##############################
-
-############################## Text editors ##############################
-# brackets
-sudo apt-get install brackets -y
-
-# atom
-sudo apt-get install atom -y
-
 # sublime-text-installer
 sudo apt-get install sublime-text-installer -y
-############################## / Text editors ##############################
 
-############################## Graphics and Image editing ##############################
-sudo apt-get install gimp -y
-sudo apt-get install darktable -y
-sudo apt-get install inkscape -y
-############################## / Graphics and Image editing ##############################
-
-# pip
-sudo apt-get install python-pip -y
-
-# virtualenv
-sudo pip install virtualenv
-
-# npm
-sudo apt-get install npm -y
-
-# synaptic
-sudo apt-get install synaptic -y
-
-# vlc
-sudo apt-get install vlc -y
-
-# java installer
-sudo apt-get install oracle-java8-installer
+# todo install anaconda 
+# todo install adapta theme
+# todo install oranchelo icon theme
+# todo do deep learning setup using this guide: https://www.pugetsystems.com/labs/hpc/Install-TensorFlow-with-GPU-Support-the-Easy-Way-on-Ubuntu-18-04-without-installing-CUDA-1170/
+# todo install npm
+# todo install java
